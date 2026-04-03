9e47b21edcf6e11a892020a9b26d8517ac028dc8
syamini05 <33005960+syamini05@users.noreply.github.com>
2026-04-04 00:49:17 +0530

    Update FirstProgram.java

--- day1/FirstProgram.java
+++ day1/FirstProgram.java
@@ -2,7 +2,7 @@ package week1.day1;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Welcome to TestLeaf");
     }
 
 }
